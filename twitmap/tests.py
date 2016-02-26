51c30c5b72d7fba80e5a7b4c89ee688348d1e8c1
cyrexin <cyrexin@gmail.com>
2016-02-26 03:44:52 -0500

    - some test cases

--- twitmap/tests.py
+++ twitmap/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
-
+from services import SearchServices
+from elastic.services import ElasticsearchServices
 # Create your tests here.
+# a = "should be\u2026"
+# print("a=%s" % a)
+
+# ss = SearchServices()
+# res = ss.insert_tweet("some contents", "rex", "1456447514096", "null")
+# print res
+
+# es = ElasticsearchServices()
+# res = es.feed_data("tweetmap", "tweets",
+#     {
+#         "contents": "the first testing tweet",
+#         "author": "rex",
+#         "timestamp": "1423423424234",
+#         "location": "New York"
+#     }
+# )
+# print res
