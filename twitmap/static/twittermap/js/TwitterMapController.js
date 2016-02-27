f81957f4f34c2d9ab5d1c0ce637ad8e43fc877b3
houliang <582192497@qq.com>
2016-02-27 15:38:08 -0500

    1. twitter map controller refactor. 2. use cluster

--- twitmap/static/twittermap/js/TwitterMapController.js
+++ twitmap/static/twittermap/js/TwitterMapController.js
@@ -3,43 +3,97 @@ var Keywordfilter = require('./Keywordfilter');
 var tweetLoader = require('./TweetLoader');
 
 var TwitterMapController = React.createClass({
-    propTypes: {
-        mapbox: React.PropTypes.object.isRequired,
-        geoJsonLayer: React.PropTypes.object.isRequired,
-    },
+  propTypes: {
+    mapbox: React.PropTypes.object.isRequired
+  },
 
-    getInitialState() {
-        return {
-            tweets: [],
-            keyword: null
-        }
-    },
-
-    onFilterValueChanged(text, value){
-        console.log('value changed: ' + value);
-        var that = this;
-        tweetLoader.loadByKeyword(value).done(function(data) {
-          console.log(data);
-          that.props.geoJsonLayer.clearLayers();
-          var scroll_id = data._scroll_id;
-          var intervalId = window.setInterval(function() {
-            tweetLoader.scroll(scroll_id).done(function(value) {
-              console.log(value);
-              if (value.hits.length === 0) {
-                window.clearInterval(intervalId);
-              } else {
-                that.props.geoJsonLayer.addData(value.hits);
-              }
-            });
-          }, 200);
-        });
-    },
-
-    render() {
-        return (
-            <Keywordfilter onChange={this.onFilterValueChanged} />
-        )
+  getInitialState() {
+    return {
+      tweets: [],
+      keyword: null
     }
+  },
+
+  componentDidMount() {
+    this.pointsLayer = null;
+  },
+
+  componentDidUpdate() {
+    this.showTweetsOnMap();
+    return true;
+  },
+
+  showTweetsOnMap() {
+    var mapbox = this.props.mapbox;
+    var tweets = this.state.tweets;
+    if (this.pointsLayer != null) {
+      mapbox.removeLayer(this.pointsLayer);
+    }
+    var clusterLayer = new L.MarkerClusterGroup({
+      iconCreateFunction: function(cluster) {
+        var html = '<div class="tweet-cluster-outer">' + 
+        '<div class="tweet-cluster">' + cluster.getChildCount() + '</div>' + 
+        '</div>';
+        return L.divIcon({
+          className: 'tweet-cluster-icon',
+          html: html
+        });
+      }
+    });
+
+    tweets.forEach(function(tweet) {
+      var coords = tweet.geometry.coordinates;
+      var url = "";
+      //var url = "http://twitter.com/" + user + "/status/" + tweet.properties.tweet_id;
+      var text = "<div class='author'><strong>" + tweet.properties.title + "</strong></div>" +
+      "<div class='date'>" + tweet.properties.datetime + "</div>" + 
+      "<div class='contents'>" + tweet.properties.description + "</div>" +
+      "<a href='" + url + "' target=_blank>View Tweet</a>" +
+      "</div>"
+      var marker = L.marker(new L.LatLng(coords[1], coords[0]), {
+        icon: L.mapbox.marker.icon({
+          'marker-color': '89C9FA',
+          'marker-size': 'medium'
+        }),
+        description: text
+      });
+      marker.bindPopup(text);
+      clusterLayer.addLayer(marker);
+    });
+
+    this.pointsLayer = clusterLayer;
+    mapbox.addLayer(this.pointsLayer);
+
+  },
+
+  onFilterValueChanged(text, value) {
+    console.log('value changed: ' + value);
+    var that = this;
+    tweetLoader.loadByKeyword(value).done(function(data) {
+      console.log(data);
+      var scroll_id = data._scroll_id;
+      var tweets = [];
+      var intervalId = window.setInterval(function() {
+        tweetLoader.scroll(scroll_id).done(function(value) {
+          console.log(value);
+          if (value.hits.length === 0) {
+            window.clearInterval(intervalId);
+            that.setState({
+              tweets: that.state.tweets.concat(tweets)
+            });
+          } else {
+            tweets = tweets.concat(value.hits);
+          }
+        });
+      }, 200);
+    });
+  },
+
+  render() {
+    return ( 
+      <Keywordfilter onChange = {this.onFilterValueChanged}/>
+    )
+  }
 });
 
 module.exports = TwitterMapController;
--- twitmap/static/twittermap/js/main.js
+++ twitmap/static/twittermap/js/main.js
@@ -6,17 +6,6 @@ var TwitterMapController = require('./TwitterMapController');
 L.mapbox.accessToken = 'pk.eyJ1IjoiaG91bGlhbmdsdiIsImEiOiJjaWwzMjNtcjEzbGdvdWdtM2c4bjNyeG1lIn0.fZxFewEvPi90HGANZnrkyA';
 var mapbox = L.mapbox.map('map', 'houlianglv.p8o27ai7');
 mapbox.setView([40.71, -74.0059], 4);
-var geoJsonLayer = L.geoJson([], {
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup(
-      "<div class='tweet-popup'>" +
-      "<div class='author'>" + feature.properties.title + "</div>" +
-      "<div class='date'>" + feature.properties.datetime + "</div>" +
-      "<div class='contents'>" + feature.properties.description + "</div>" +
-      "</div>"
-    );
-  }
-}).addTo(mapbox);
 
-ReactDOM.render(<TwitterMapController mapbox={mapbox} geoJsonLayer={geoJsonLayer} />, 
+ReactDOM.render(<TwitterMapController mapbox={mapbox} />, 
   document.getElementById('twittermap-controller'));
