730707d77ccf9cecbfda2827d8ade78b22c5c838
houliang <582192497@qq.com>
2016-02-27 15:43:29 -0500

    add config file

---
+++ twitmap/static/twittermap/js/config.js
@@ -0,0 +1,8 @@
+var config = {
+	map: {
+		accesstoken: "YOUR MAPBOX ACCESS TOKEN",
+		mapId: "YOUR MAP ID"
+	}
+};
+
+module.exports = config;
--- twitmap/static/twittermap/js/main.js
+++ twitmap/static/twittermap/js/main.js
@@ -1,10 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var TwitterMapController = require('./TwitterMapController');
+var config = require('./config');
 
-
-L.mapbox.accessToken = 'pk.eyJ1IjoiaG91bGlhbmdsdiIsImEiOiJjaWwzMjNtcjEzbGdvdWdtM2c4bjNyeG1lIn0.fZxFewEvPi90HGANZnrkyA';
-var mapbox = L.mapbox.map('map', 'houlianglv.p8o27ai7');
+L.mapbox.accessToken = config.map.accesstoken;
+var mapbox = L.mapbox.map('map', config.map.mapId);
 mapbox.setView([40.71, -74.0059], 4);
 
 ReactDOM.render(<TwitterMapController mapbox={mapbox} />, 
