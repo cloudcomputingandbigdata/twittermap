e1612ac7579f944d37b5688128877535ab6b8595
houliang <582192497@qq.com>
2016-02-27 00:13:49 -0500

    remove old code

--- twitmap/static/twittermap/js/main.js
+++ twitmap/static/twittermap/js/main.js
@@ -28,34 +28,6 @@ $('#select').dropdown({
 
 
 L.mapbox.accessToken = 'pk.eyJ1IjoiaG91bGlhbmdsdiIsImEiOiJjaWwzMjNtcjEzbGdvdWdtM2c4bjNyeG1lIn0.fZxFewEvPi90HGANZnrkyA';
-/*var geojson = [{
-  "type": "Feature",
-  "geometry": {
-    "type": "Point",
-    "coordinates": [-77.03238901390978, 38.913188059745586]
-  },
-  "properties": {
-    "title": "Mapbox DC",
-    "description": "1714 14th St NW, Washington DC",
-    "marker-color": "#fc4353",
-    "marker-size": "large",
-    "marker-symbol": "monument"
-  }
-}, {
-  "type": "Feature",
-  "geometry": {
-    "type": "Point",
-    "coordinates": [-122.414, 37.776]
-  },
-  "properties": {
-    "title": "Mapbox SF",
-    "description": "155 9th St, San Francisco",
-    "marker-color": "#fc4353",
-    "marker-size": "large",
-    "marker-symbol": "harbor"
-  }
-}];*/
-
 var mapbox = L.mapbox.map('map', 'houlianglv.p8o27ai7');
 mapbox.setView([40.71, -74.0059], 4);
 var geoJsonLayer = L.geoJson([], {
